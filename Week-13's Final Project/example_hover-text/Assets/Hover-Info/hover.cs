2a7e254dd1a3f2f7e3c9069da4d3a7ff1798ad16
atruong129 <student@ent-v314-007.local>
2017-12-12 15:49:00 -0500

    "Progress for the finals"

--- Week-13's Final Project/example_hover-text/Assets/Hover-Info/hover.cs
+++ Week-13's Final Project/example_hover-text/Assets/Hover-Info/hover.cs
@@ -5,18 +5,33 @@ using UnityEngine;
 public class hover : MonoBehaviour {
 
 	public string HoverText;
+
+	public string SecondaryText;
+
 	public TextMesh DisplayTextObject;
 
-	void OnMouseEnter() {
+	public TextMesh SecondaryDisplayTextObject;
+
+	public GameObject myImage;
+
+	void OnMouseDown() {
 		// switch the 3d text so that its text value
 		// is now whatever text value this hover instance holds
 		DisplayTextObject.text = HoverText;
 
+		SecondaryDisplayTextObject.text = SecondaryText;
+
 		// show it
 		DisplayTextObject.color = Color.white;
+		SecondaryDisplayTextObject.color = Color.white;
+
+		myImage.SetActive (true);
 	}
 
 	void OnMouseExit() {
 		DisplayTextObject.color = new Color(0,0,0,0);
+		SecondaryDisplayTextObject.color = Color.white;
+
+		myImage.SetActive (false);
 	}
 }
